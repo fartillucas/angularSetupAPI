6cb78337fc7bd6941282dde291bfd55bc9e2fe15
michael vittus <fartillucas@gmail.com>
2020-11-17 10:55:10 +0100

    colored update confirmation

--- ui/angular10/src/app/app.component.ts
+++ ui/angular10/src/app/app.component.ts
@@ -23,14 +23,27 @@ export class AppComponent implements OnInit {
   }
   constructor(private service: SharedService) { }
   updateList: any = [];
+  k = 1;
+
+  yesterday(k) {
+    var day1 = new Date();
+    day1.setDate(day1.getDate() - k);
+    return day1.toISOString().slice(0, 10);
+  }
 
 
 
   refreshDate() {
     this.service.getDateOfUpdateList().subscribe(data => {
       this.updateList = data;
-      console.log(this.updateList);
+      if (this.updateList[0].Date.toString().split("T00:00:00")[0] == this.yesterday(1)) {
+        document.getElementById("timeCheck").style.backgroundColor = "#4CAF50";
+      } else {
+        document.getElementById("timeCheck").style.backgroundColor = "#ff0000";
+
+      }
     })
+
   }
 
 }
